3e8e7d7049b19a9b5b07712af000157262ec8601
genloga <genloga@gmail.com>
2017-05-11 12:02:03 +0000

    Add of the pages folder + modification of the server.js file

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var http = require('http');
+var fs = require('fs')
 var server = http.createServer();
 
 server.on('request', (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write('Je s\'appelle #Groot :-) ')
-    res.end()
+     fs.readFile('pages/index.html', (err , data) => {
+    if (err){
+	res.write('Erreur :'+err)
+ 	res.end()
+    } else {
+	res.writeHead(200, (request, response) => {
+       response.writeHead({
+	'Content-Type': 'text/html; charset=UTF-8'	
+	})
+     })
+    res.end(data)
+    }
+  })
 })
 server.listen(8080)
