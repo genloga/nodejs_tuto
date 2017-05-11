19f7b9dcbb42b82e326b37efc1c6c71ef3f77743
genloga <genloga@gmail.com>
2017-05-11 12:28:43 +0000

    Ajout d'un parametre a l'url et comment l'exploiter

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var http = require('http');
 var fs = require('fs')
-var server = http.createServer();
+var url = require('url')
 
+var server = http.createServer();
 server.on('request', (req, res) => {
-     fs.readFile('pages/index.html', (err , data) => {
+     fs.readFile('pages/index.html','UTF-8', (err , data) => {
     if (err){
 	res.write('Erreur :'+err)
  	res.end()
     } else {
 	res.writeHead(200, (request, response) => {
        response.writeHead({
-	'Content-Type': 'text/html; charset=UTF-8'	
+	'Content-Type': 'text/html'	
 	})
      })
+    var query = url.parse(req.url, true).query
+    var nom = query.name === undefined ? 'anonyme' : query.name
+    var data = data.replace('{{ name }}' , nom)
     res.end(data)
     }
   })
